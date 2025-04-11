444a89d4b1fae5ab27862e616eab836090789f05
Drackout <mikatx2@hotmail.com>
2025-04-11 13:24:19 +0100

    First commit

---
+++ LP1SpeedType/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LP1SpeedType
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
